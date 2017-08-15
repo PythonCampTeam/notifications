c222d87265ef7230f5d35edcfa63a3a44ff10bea
Tamara Malysheva <tamara.malysheva@saritasa.com>
2017-08-15 19:38:16 +0700

    update notifications body

--- rpc/notifications.py
+++ rpc/notifications.py
@@ -3,11 +3,12 @@ import sendgrid
 from sendgrid.helpers.mail import Mail, Email, Content
 # import urllib.request as urllib
 from twilio.rest import Client
+from nameko.timer import timer
 # from datetime import datetime
-#from keys import SENDGRID_API_KEY
+# from keys import SENDGRID_API_KEY
 # import json
 # from nameko.events import EventDispatcher, event_handler
-# from config.settings.common import security
+#from notifications.config.settings.common import security as security_settings
 
 
 class Notifications(object):
@@ -15,12 +16,7 @@ class Notifications(object):
         """
     name = 'NotificationsRPC'
 
-    #sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
-
-    # def __init__(self):
-    #     self.sendgrid_key = SENDGRID_API_KEY
-    #     #self.sendgrid_v2_client = sendgrid.SendGridAPIClient(self.sendgrid_key)
-    #     self.sendgrid_v3_client = sendgrid.SendGridAPIClient(apikey=self.sendgrid_key)
+    # sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
 
     @rpc
     def testing(self, **kwargs):
@@ -29,20 +25,22 @@ class Notifications(object):
                 'docs': self.__class__.__doc__}
 
     @rpc
-    def send_email(self, to_email='test@example.com'):
-        """This method send email to customer
+    #@timer(interval=2)
+    def send_email(self, to_email, from_email, subject, content):
+        """This method send email to customer with use SenfGrid
         Args:
-            to_email (str) : email of customer
+            to_emails (str) : email of customer
+            from_emails(str): email of shop
+            subject (str): subject of mail
+            content(str): content of the mail
         """
         SENDGRID_API_KEY = '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'
         sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
-        from_email = Email("test@example.com")
-        #to_email = Email("test@example.com")
+        from_email = Email(from_email)
         to_email = Email(to_email)
-        subject = "Notifications about Order"
-        content = Content("text/plain", "Orders")
         mail = Mail(from_email, subject, to_email, content)
-        #response = sg.client.mail.send.post(request_body=mail.get())
+        content = Content(content)
+        # response = sg.client.mail.send.post(request_body=mail.get())
         sg.client.mail.send.post(request_body=mail.get())
         # print(response.status_code)
         # print(response.body)
@@ -50,39 +48,21 @@ class Notifications(object):
         return "Messages send"
 
     @rpc
-    def send_sms(self, number):
+    #@timer(interval=2)
+    def send_sms(self, to_phone, content):
         """This method send sms to customer
         Args:
-            number (str) : number of customer
+            to_phone (str) : number of customer
+            body (str): message to customer
+            from(str): number of salary
         """
         accaunt_sid = 'AC3adbfe0e72f9d7dc7197fefd2cab7aca'
         auth_token = 'f3ab11d8839c7752d07db9854b93bc8f'
-        client = Client(accaunt_sid, auth_token)
-        client.messages.create(
-                to=number,
-                from_='+16195866444',
-                body='Na potolke nosok!'
-                )
-        return 'message send'
 
-    # @rpc
-    # def send_email2(self, to_email, from_email, subject, body_html, body_text):
-    #
-    #     message = sendgrid.Mail(to=to_email, subject=subject, html=body_html,
-    #                             text=body_text, from_email=from_email)
-    #     print("SENDGRID: send_email: Attempting to send email to {to_email} from {from_email}
-    #     with subject {subject} at time {time}".format(
-    #         to_email=to_email,
-    #         from_email=from_email,
-    #         subject=subject,
-    #         time=datetime.now(),
-    #         ))
-    #     status, msg = self.sendgrid_v3_client.send(message)
-    #     response = Notifications.sg.client.mail.send.post(message)
-    #     print("SENDGRID: send_email: Received response from Sendgrid at time {time} with status {status}
-    #      and response {msg}".format(
-    #         time=datetime.now(),
-    #         status=status,
-    #         msg=msg,
-    #         ))
-    #     return response
+        client = Client(accaunt_sid, auth_token)
+        message = client.messages.create(
+                to=to_phone,
+                from_='+16195866444',
+                body=content
+        )
+        #return(message.error_code, message.error_message)
