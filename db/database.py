a075c9c923cdf34f2e8b57a29b9d940e73160a44
Tamara Malysheva <tamara.malysheva@saritasa.com>
2017-08-18 15:40:40 +0700

    email send status code

---
+++ db/database.py
@@ -0,0 +1,77 @@
+import operator
+import time
+
+
+class StoreDB(object):
+    """
+        class for storing and sorting data from micro services
+        Methods:
+            __iadd__: function append store
+                Args:
+                    other(dict): information to save in db
+                Return:
+                    self
+            update(dict):
+                Args:
+                     address_to(dict): shipment address information
+                Return:
+                    self.db
+            delete(dict):
+                Args:
+                    address_to(dict): shipment address information for delete
+                Return:
+                    self.db(list)
+            lower:
+                Args:
+                    sort(str): string for sorted lower output from db
+                Return:
+                    list
+            upper:
+                Args:
+                    sort(str): string for upper sorted output from db
+                Return:
+                    list
+            get:
+                Args:
+                    object_id(str): string of object id
+                Return:
+                    dict
+    """
+    def __init__(self, **kwargs):
+        self.db = {}
+        self.data_stored = kwargs.get('data_stored')
+        self.data_key = kwargs.get('data_key')
+
+    def add(self, other):
+        object_id = other.get(self.data_key, str(time.time()))
+        if isinstance(other, dict):
+            if other not in self.db.items():
+                self.db[object_id] = other
+        return self
+
+    def update(self, **kwargs):
+        item = kwargs.get(self.data_stored)
+        object_id = item.get(self.data_key)
+        if self.db[object_id]:
+            self.db[object_id].update(item)
+        return self
+
+    def delete(self, **kwargs):
+        object_id = kwargs.get(self.data_key)
+        if any(object_id in _ for _ in self.db):
+            del self.db[object_id]
+        return self.db
+
+    # TODO change name methods
+    def sorting_items(self, sort='name', reverse=False):
+        if any(sort in _ for _ in self.db.values()):
+            return sorted(self.db.values(),
+                          key=operator.itemgetter(sort),
+                          reverse=reverse)
+
+    def get_item(self, **kwargs):
+        item = kwargs.get('object_id')
+        return self.db[item]
+
+    def get_items(self):
+        return self.db.items()
--- rpc/mail.py
+++ rpc/mail.py
@@ -1,17 +1,17 @@
 
 import sendgrid
-import os
+
 from sendgrid.helpers.mail import Mail, Email, Content
 
 
-SENDGRID_API_KEY = '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'
+SENDGRID_API_KEY = ''
 sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
 from_email = Email("test@example.com")
-to_email = Email("varvara.malysheva@saritasa.com")
+to_email = Email("tamara.malysheva@saritasa.com")
 subject = "Sending mail"
 content = Content("text/plain", "Hello from Russia")
 mail = Mail(from_email, subject, to_email, content)
 response = sg.client.mail.send.post(request_body=mail.get())
 print(response.status_code)
-print(response.body)
-print(response.headers)
+#print(response.body)
+#print(response.headers)
