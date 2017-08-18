84009bbe901278e0d97f548297728c7f24597cca
Tamara Malysheva <tamara.malysheva@saritasa.com>
2017-08-18 15:41:50 +0700

    no sengrid api key

--- config/settings/common/security.py
+++ config/settings/common/security.py
@@ -3,5 +3,4 @@ TOKEN_GOSHIPPO = {'LIVE_TOKEN': 'shippo_live_6a3b0e01b47f7e814bf7b74c35561703aae
                   'TEST_TOKEN': 'shippo_test_55dfc05531b49ed2e711fa2cca863d72c68f87b7'}
 accaunt_sid = 'AC3adbfe0e72f9d7dc7197fefd2cab7aca'
 auth_token = 'f3ab11d8839c7752d07db9854b93bc8f'
-SENDGRID_API_KEY = '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'
 twilio_number = '+16195866444'
