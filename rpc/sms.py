7a0252fed78b35ffdf05c4c2a77fc3350dbd35cf
Tamara Malysheva <tamara.malysheva@saritasa.com>
2017-08-22 19:10:05 +0700

    fix conflict

--- rpc/sms.py
+++ rpc/sms.py
@@ -7,12 +7,12 @@ schema_sms = {"to_phone": {'type': 'string'},
               "content": {'type': 'string'}}
 
 # @rpc
-# def buy():
-#
-#     client = Client(accaunt_sid, auth_token)
-#
-#     client.messages.create(
-#         to='+77017335394',
-#         from_='+16195866444',
-#         body='Na potolke nosok!'
-#         )
+def buy():
+
+    client = Client(accaunt_sid, auth_token)
+
+    client.messages.create(
+        to='+79994413746',
+        from_='+16195866444',
+        body='Na potolke nosok!'
+        )
