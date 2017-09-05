66c4ab59095cd5e978deea8230a287934a33c159
Tamara Malysheva <tamara.malysheva@saritasa.com>
2017-09-05 20:02:41 +0700

    update template

--- rpc/content.py
+++ rpc/content.py
@@ -12,19 +12,21 @@ def email_content(name, label=None, order=None):
     Return:
         content (html): Temaplate of mail.
     """
-    ENV = Environment(loader=PackageLoader(
-                                    'notifications.config',
-                                    'templates'
-                                          )
-                      )
-    template = ENV.get_template('email_template.html')
-    context = {
-                'name': name,
-                'label': label,
-                'order_id': order.get('id'),
-                'order_items': order.get('items')
-               }
-    content = template.render(context)
+    if order:
+        ENV = Environment(loader=PackageLoader(
+                                        'notifications.config',
+                                        'templates'
+                                              )
+                          )
+        template = ENV.get_template('email_template.html')
+        context = {
+                    'name': name,
+                    'label': label,
+                    'order_id': order.id,
+                    'order_items': order['items']
+                   }
+        content = template.render(context)
 
-    content = Content('text/html', content)
-    return content
+        content = Content('text/html', content)
+        return content
+    return None
